53f9edaf7242582b94e7960431054f55bc9f5bee
Praful Parmar <prafulparmar095@gmail.com>
2025-06-20 20:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const button = document.getElementById('enterBtn');
+    const statusElement = document.getElementById('status');
+
+    button.addEventListener('click', () => {
+      // Replace the <p> element with an <h1> element
+      const h1 = document.createElement('h1');
+      h1.id = 'status';
+      h1.textContent = 'Entered Metaverse';
+      statusElement.replaceWith(h1);
+    });
