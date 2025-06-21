2d17496ab3a7041ae0c7a203aa49df02e500c8c6
Praful Parmar <prafulparmar095@gmail.com>
2025-06-21 08:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const button = document.getElementById('enterBtn');
-    const statusElement = document.getElementById('status');
-
-    button.addEventListener('click', () => {
-      // Replace the <p> element with an <h1> element
-      const h1 = document.createElement('h1');
-      h1.id = 'status';
-      h1.textContent = 'Entered Metaverse';
-      statusElement.replaceWith(h1);
-    });
+document.getElementById("enterBtn").addEventListener("click", function() {
+      const p = document.getElementById("status");
+      const h1 = document.createElement("h1");
+      h1.id = p.id; 
+      h1.textContent = "Entered Metaverse";
+      p.replaceWith(h1);
